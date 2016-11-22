e4610709cf8cb3410d4e37d4ff87b0788b0cb4ca
Wei Ren <renwei@smartconn.cc>
2016-11-22 00:32:32 -0800

    If the token is not found in the input string, strtok will actually return the entire string.
    Change-Id: I03313ade3f37c12c2778f083d40d7d4e0d99992d
    Signed-off-by: Wei Ren <renwei@smartconn.cc>

--- makefile-example/main.cpp
+++ makefile-example/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include "functions.h"
 
 using namespace std;
 
-int main(){
+int main()
+{
+    char str[] = "ping";
+    char *pch = NULL;
+    pch = strtok(str, "`");
+    if (pch == NULL)
+    {
+        printf("pch is NULL and the token is not found.\n");
+    }
+    else
+    {
+        printf("pch = %s is not NULL and the token is found.\n", pch);
+    }
+
     print_hello();
     cout << endl;
     cout << "The factorial of 5 is " << factorial(5) << endl;
